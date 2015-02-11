87179658a0198e2b1da77a27dc8862c72246af6c
jack <jack@jack-Inspiron-5521>
2015-02-10 23:33:24 -0800

    localserver post

--- src/main/java/com/itu/myserver/Localserver_Posting.java
+++ src/main/java/com/itu/myserver/Localserver_Posting.java
@@ -9,19 +9,32 @@ import org.apache.log4j.Logger;
 
 import com.itu.DAO.DataAccess;
 import com.itu.bean.SmartMeterData;
+import com.itu.localserver.client.SmartMeterDataProtos.SmartMeter;
 import com.itu.util.Log4jUtil;
 
 
 @Path("/localserverpost")
 public class Localserver_Posting {
 	Logger logger= Log4jUtil.getLogger(AddressBookResource.class);
-	//@Path("/smartmeterdata")
+	@Path("/smartmeter")
 	@POST
 	@Consumes("application/x-protobuf")
-	public Response postPerson(SmartMeterData smdata) {		
-		logger.debug(String.format("post a smdata, id:%d, ieee:%s, energy:%s", smdata.getId(),smdata.getSmIeeeAddress(),smdata.getEnergy()));
+	public Response postSmartMeter(SmartMeter smdata) {		
+		logger.debug(String.format("post a smdata, id:%d, ieee:%s, power:%s", smdata.getId(),smdata.getIeeeAddress(),smdata.getPower()));
 		//AddressBookStore.store(person);
-		DataAccess.localserver_postSmartmeterData(smdata);
+		DataAccess.localserver_postSmartmeter(smdata);
+		return Response.ok().build();
+	}
+	
+	
+	@Path("/smartmeterdata")
+	@POST
+	@Consumes("application/x-protobuf")
+	public Response postSmartMeterData(com.itu.localserver.client.SmartMeterDataProtos.SmartMeterData smdatalist) {	
+		
+		//logger.debug(String.format("post a smdata, id:%d, ieee:%s, power:%s", smdata.getId(),smdata.getIeeeAddress(),smdata.getPower()));
+		//AddressBookStore.store(person);
+		DataAccess.localserver_postSmartmeterData(smdatalist);
 		return Response.ok().build();
 	}
 }
